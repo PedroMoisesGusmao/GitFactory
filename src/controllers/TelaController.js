43c5ed62470e6359ba16eb2116aea6ec34e677bb
Pedro <pedroaraujo4910@gmail.com>
2024-06-05 12:21:45 -0300

    Adicionando header do cors e consoles logs

--- src/controllers/TelaController.js
+++ src/controllers/TelaController.js
@@ -15,7 +15,7 @@ class TelaController{
         } else {
             telas = await Tela.find();
         }
-
+        console.log("GET realizado com sucesso.")
         return res.json(telas);
     }
 
@@ -25,13 +25,16 @@ class TelaController{
             if (tipo === 'dialogo' || tipo === 'final') {}
             else if (tipo === 'exemplo') {
                 if (!req.body.exemplo) {
+                    console.log("Atributo 'exemplo' não foi informado na tela 'exemplo'");
                     return res.status(400).json({ error: "Atributo 'exemplo' é obrigatório para tela de tipo 'exemplo'" });
                 }
             } else if (tipo === 'terminal') {
                 if (!req.body.resposta || !req.body.desafio) {
+                    console.log("Atributo 'resposta' e 'desafio' não foram informados na tela de tipo'exemplo'");
                     return res.status(400).json({ error: "Atributo 'resposta' e 'desafio' são obrigatórios para tela de tipo 'terminal'"});
                 }
             } else {
+                console.log("Atributo 'tipo' não foi passado.");
                 return res.status(400).json({ error: "Atributo 'tipo' precisa ser uma string entre 'dialogo', 'exemplo', 'terminal' e 'final'"})
             }
 
@@ -47,6 +50,7 @@ class TelaController{
                 }
             }
             if (erros.length > 0) {
+                console.log(`Atributos ${erros} não localzados`);
                 return res.status(400).json({
                     error: `Os campos ${erros.join(', ')} não corresponderam aos atributos obrigatórios.`,
                     atributosObrigatorios
@@ -54,6 +58,7 @@ class TelaController{
             }
             const find = await Tela.find({grupoTela, tipo});
             if (find.length > 0) {
+                console.log("Tela já foi cadastrada.")
                 return res.status(400).json({error: "Essa tela já foi inserida"});
             }
             const insercao = {
@@ -74,8 +79,10 @@ class TelaController{
                 insercao.exemplo = req.body.exemplo;
             }
             const tela = await Tela.create(insercao);
+            console.log("POST realizado com sucesso.");
             return res.json({ tela });
         } catch (e) {
+            console.log("Atributo com tipagem errada.")
             return res.status(400).json({ error: "Atributo com tipagem errada passado"});
         }
     }
