707a5d5a9f6584f0aaf1f0e79224afc40e0621f0
Pedro <pedroaraujo4910@gmail.com>
2024-06-06 11:17:07 -0300

    Removendo cors

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,4 @@ routes.get('/tela', headerCors,  TelaController.index);
 
 routes.post('/tela', headerCors, TelaController.store);
 
-function headerCors(req, res, next) {
-    res.set("Access-Control-Allow-Origin", "127.0.0.1:5500");
-    next();
-}
-
 export default routes;
